e8bafa5e740e298721f592833a44d9e008bc7014
chanikul <chanikul@me.com>
2025-05-11 23:32:22 +0100

    Make payment status page dynamic

--- src/app/payment-status/page.tsx
+++ src/app/payment-status/page.tsx
@@ -3,6 +3,8 @@
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 
+export const dynamic = 'force-dynamic';
+
 export default function PaymentStatus() {
   const searchParams = useSearchParams();
   const error = searchParams?.get('error') || null;
