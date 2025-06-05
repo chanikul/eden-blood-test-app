890345ced68e3a1eb32c7890875aa729fcb288fb
chanikul <chanikul@me.com>
2025-06-05 23:36:53 +0100

    Simplify Netlify configuration to rely on official Next.js plugin for asset handling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import fs from 'fs'
+import path from 'path'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Force CSS to be loaded properly */}
-        <link rel="stylesheet" href="/_next/static/css/app.css" />
+        {/* Meta tags */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Eden Clinic - Blood Testing Services" />
+        
+        {/* Preconnect to domains */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </Head>
       <body>
         <Main />
